ce56cb803c29c017d481ecf0c7683b4ebe0fde43
kellymporta <kelly.arellano9163@alumnos.udg.mx>
2026-08-13 11:47:02 -0600

    ListaCompras

---
+++ DEV.F/Intro_React/shopping/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import ListaCompras from "./components/ListaCompras/ListaCompras";
+
+function App() {
+  
+
+  return (
+    <div>
+      <ListaCompras />
+    </div>
+  );
+}
+
+export default App
